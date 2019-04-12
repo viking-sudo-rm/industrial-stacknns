3abe77ce9a97fe5937026b99804be6015841bfa5
Will <william.merrill@yale.edu>
2019-04-12 04:42:11 -0400

    Parsing adapted to work on Linzen.

--- predict_trees.py
+++ predict_trees.py
@@ -6,27 +6,25 @@ from data_readers.brown import BrownDatasetReader
 from predictor import TreePredictor
 from stack_rnn_LM import StackRNNLanguageModel
 
-def brown_predict(sentence):
-    vocab = Vocabulary.from_files("saved_models/vocabulary-brown")
-    model = StackRNNLanguageModel(vocab, rnn_dim=100, stack_dim=16)
-
-    with open("saved_models/stack-brown.th", "rb") as fh:
-        model.load_state_dict(torch.load(fh))
-
+def predict(model, sentence):
     dataset_reader = BrownDatasetReader(labels=False)
     predictor = TreePredictor(model, dataset_reader)
-    return predictor.predict(sentence)
+    prediction = predictor.predict(sentence)
+    pop_strengths = prediction["pop_strengths"]
+    pairs = list(zip(sentence.split(" "), pop_strengths))
+    return greedy_parse(pairs)
 
 
 def main():
-    sentence = "AT NN IN AT NNS VBD AT JJ NN"
-    prediction = brown_predict(sentence)
-    pop_strengths = prediction["pop_strengths"]
+    # sentence = "AT NN IN AT NNS VBD AT JJ NN"
+    sentence = "John and Jill told Mary that she was cool"
 
-    print("Parsing..")
-    pairs = list(zip(sentence.split(" "), pop_strengths))
-    parse = greedy_parse(pairs)
-    print("Got parse", parse)
+    vocab = Vocabulary.from_files("saved_models/vocabulary-linzen")
+    model = StackRNNLanguageModel(vocab, rnn_dim=100, stack_dim=16)
+    with open("saved_models/stack-linzen.th", "rb") as fh:
+        model.load_state_dict(torch.load(fh))
+
+    print(predict(model, sentence))
 
 
 if __name__ == "__main__":
